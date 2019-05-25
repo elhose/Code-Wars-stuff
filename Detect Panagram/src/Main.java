f15756b67e2dd7063b90d9e670533846ab4a8079
elhose <51024177+elhose@users.noreply.github.com>
2019-05-25 16:06:19 +0200

    Update Main.java

--- Detect Panagram/src/Main.java
+++ Detect Panagram/src/Main.java
@@ -1,12 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
         System.out.println(check("The quick brown fox jumps over the lazy dog.!!!!!!"));
-
     }
-
-
 
     public static boolean check(String sentence){
         char[] array = new char[26];
